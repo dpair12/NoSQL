164d5677a3290d1cc587feb949cc9ad3663e56e9
D Pair <DESEANPAIR12@GMAIL.COM>
2024-04-22 02:52:57 -0400

    Routes

---
+++ routes/api/UserRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+const {
+getUsers,
+getSingleUser,
+createUser,
+UpdateUser,
+DeleteUser,
+addFriend,
+DeleteFriend,
+} = require('../../controllers/UserController');
+
+// api/users
+router.route('/').get(getUsers);
+router.route('/').post(createUser);
+
+// api/users/:userId
+router.route('/:userId').get(getSingleUser);
+router.route('/:userId').put(UpdateUser);
+router.route('/:userId').delete(DeleteUser);
+
+// api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(DeleteFriend);
+
+module.exports = router;
